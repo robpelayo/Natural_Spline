25f2a6c3f9c31703a3398a9d4c05ba6c1dbb2cff
Robert Pelayo <rpelayo@pdx.edu>
2019-12-02 21:31:57 -0800

    Completed tri diagonal form

--- PRlab5.c
+++ PRlab5.c
@@ -10,7 +10,7 @@ void print_matrix (double m[M][M+1], int n)
 
   for (r = 0 ; r < n ; r++) {
     for (c = 0 ; c <= n ; c++) {
-      printf("%.16lf ",m[r][c]) ;
+      printf("%.2f ",m[r][c]) ;
     }
     printf("\n") ;
 
@@ -27,7 +27,7 @@ int main()
   double X,Y ;
   int num_of_points = 0;
   double terms[100];
-
+  /*(
   // must do this before you do 'almost' any other graphical tasks 
   swidth = 830 ;  sheight = 800 ;
   G_init_graphics (swidth,sheight) ;  // interactive graphics
@@ -41,7 +41,6 @@ int main()
   G_rgb (0.0, 0.0, 1.0) ; // blue
   lowleftx = 800; lowlefty = 0 ; width = 30 ; height = 799 ;
   G_fill_rectangle (lowleftx, lowlefty, width, height) ;
-
   // Get all the points the user wants, terminate when they click above 
   // 800 (blue bar)
   G_rgb(1,1,0) ;
@@ -54,32 +53,102 @@ int main()
     ++num_of_points;
   }
   G_rgb(1,0,0) ;
+  */
   
   // intialize
   double matrix[M][M+1];
   for (int i = 0; i < M; ++i)
     for (int j = 0; j <= M; ++j)
       matrix[i][j] = 0;
+  num_of_points = 5;
   int num_of_vars = (num_of_points - 1) * 2;
-  int current = 0;
+  int current = 1;
+  int start = 0;
   bool first = true;
-  // 6 points will yield 5 splines (10 variables)
+  // 6 points will yield 5 splines (10 variables) so 11 columns
   // A and B are the line variables
-  // special cases
+
+  // (3,4)
+  x[0] = 3;
+  y[0] = 4;
+  // (4,6)
+  x[1] = 4;
+  y[1] = 6;
+  // (6,5)
+  x[2] = 6;
+  y[2] = 5;
+  // (7,8);
+  x[3] = 7;
+  y[3] = 8;
+  // (9,10)
+  x[4] = 9;
+  y[4] = 10;
+
   matrix[0][0] = 2;
-  matrix[0][1] = -2 * (x[current - 1] - x[current]);
+  matrix[0][1] = -2 * (x[1] - x[0]);
   matrix[0][num_of_vars] = 0;
 
-  for (int i = 1; i < num_of_vars; ++i){
-    for (int j = 0; j < num_of_vars + 1; ++j){
+  //printf("matrix[0][0]: %lf\n", matrix[0][0]);
+  for (int i = 1; i < num_of_vars - 1; ++i, ++start){
+    for (int j = start; j < num_of_vars + 1; ++j){
+      // i = 0
       if (first){
-        
+        if (j == start){
+          matrix[i][j] = x[current] - x[current-1];
+          printf("x[%i]: %lf   x[%i]: %lf\n", current, x[current], current-1, x[current-1]);
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else if (j == start + 1){
+          matrix[i][j] = pow(x[current] - x[current-1], 2);
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else if (j == start + 2){
+          matrix[i][j] = x[current+1] - x[current];
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else if (j == num_of_vars){
+          matrix[i][j] = ((y[current+1]-y[current])/(x[current+1]-x[current])) - ((y[current]-y[current-1])/(x[current]-x[current-1]));
+          printf("endmatrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else{
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+          continue;
+        }
       }
-      matrix[i][j] = 0;
+      // i = 1
+      else{
+        if (j == start){
+          matrix[i][j] = pow(x[current] - x[current-1], 2);
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else if (j == start + 1){
+          matrix[i][j] = -(x[current + 1] - x[current -1]);
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else if (j == start + 2){
+          matrix[i][j] = (x[current] - x[current -1]) * (x[current +1] - x[current]);
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else if (j == num_of_vars){
+          matrix[i][j] = -matrix[i-1][j];
+          printf("endmatrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+        }
+        else 
+          printf("matrix[%i][%i]: %lf\n", i, j, matrix[i][j]);
+          continue;
+      }
     }
-
+    if (first == true)
+      first = false;
+    else {
+      first = true;
+      ++current;
+    }
   }
 
+  matrix[num_of_vars-1][num_of_vars-2] = 1;
+  matrix[num_of_vars-1][num_of_vars-1] = -2 * (x[num_of_points-1] - x[num_of_points-2]);
+  matrix[num_of_vars-1][num_of_vars] = 0;
   print_matrix(matrix, num_of_vars);
 
   // graph the function using Y = ax^0 + bx^1 + cx^2...
